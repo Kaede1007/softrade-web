051bbb94601ce73b167363373a11f7cb7cdd97b5
Kaede1007 <oneoks45@gmail.com>
2026-02-17 14:00:16 +0900

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,30 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/request";
+export const config = {
+  matcher: ["/(.*)"], // すべてのページに適用
+};
 
-export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get("authorization");
+export default function middleware(req: Request) {
+  const authHeader = req.headers.get("authorization");
 
-  if (basicAuth) {
-    const authValue = basicAuth.split(" ")[1];
+  if (authHeader) {
+    const authValue = authHeader.split(" ")[1];
     const [user, pwd] = atob(authValue).split(":");
 
-    // ユーザー名とパスワードを環境変数から取得
+    // 環境変数に設定した値と照合
     if (
       user === process.env.BASIC_AUTH_USER &&
       pwd === process.env.BASIC_AUTH_PASSWORD
     ) {
-      return NextResponse.next();
+      return new Response(null, {
+        status: 200,
+        headers: { "x-middleware-next": "1" },
+      });
     }
   }
 
-  return new NextResponse("Auth Required", {
+  return new Response("Auth Required", {
     status: 401,
     headers: {
       "WWW-Authenticate": 'Basic realm="Secure Area"',
     },
   });
 }
-
-// 認証を適用するパスを指定（ここでは全ページ）
-export const config = {
-  matcher: ["/:path*"],
-};
